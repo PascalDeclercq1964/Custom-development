45eadb550ec7188ff22ac00618028e82f0633696
PascalDeclercq1964 <info@pascaldeclercq.be>
2025-11-01 16:58:50 +0100

    first commit

---
+++ purchase_to_Sale_reinvoice/__manifest__.py
@@ -0,0 +1,15 @@
+{
+'name': 'Purchase to Sale Reinvoice',
+'version': '14.0.1.0.1',
+'summary': 'Mass-reinvoice vendor bills to one fixed customer',
+'author': 'Generated for user',
+'license': 'AGPL-3',
+'category': 'Accounting',
+'depends': ['account'],
+'data': [
+'security/ir.model.access.csv',
+'views/reinvoice_views.xml',
+],
+'installable': True,
+'application': False,
+}
