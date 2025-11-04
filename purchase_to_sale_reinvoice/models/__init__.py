59cd2ae376019d28da425b41ad45b13d5bb3c770
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2025-11-04 08:44:27 +0100

    #2

--- purchase_to_sale_reinvoice/models/__init__.py
+++ purchase_to_sale_reinvoice/models/__init__.py
@@ -1,2 +1,4 @@
-from . import account_move
-from . import res_config_settings
+from . import account_move        # Voor de doorfacturatie logica
+from . import res_config_settings # Voor de vaste klant instelling
+from . import account_account     # 👈 Uw nieuwe bestand voor account mapping
+from . import account_tax         # 👈 Uw nieuwe bestand voor tax mapping
