a4c55ebc8cab90a8ebfe68977aa02f3c5efb9725
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2025-11-19 08:43:20 +0100

    02

--- B2B/controllers/main.py
+++ B2B/controllers/main.py
@@ -1,51 +1,35 @@
-from odoo import http
-from odoo.http import request
-from odoo.addons.website_sale.controllers.main import WebsiteSale
-import logging
+# controllers/main.py (Gewijzigde _set_b2b_pricelist_context)
 
-_logger = logging.getLogger(__name__)
-
-class WebsiteSaleB2B(WebsiteSale):
-
-    def _set_b2b_pricelist_context(self):
-        """ Hulpfunctie om de B2B-prijslijst in de context te injecteren. """
+def _set_b2b_pricelist_context(self):
+    """ Hulpfunctie om de B2B-prijslijst in de context te injecteren. 
+        Leest partnergegevens met sudo om ACL-fouten te voorkomen, 
+        maar gebruikt de ID van de aangemelde gebruiker.
+    """
+    if request.website.id == 2 and request.env.user:
+        user = request.env.user
         
-        # Controleer of we op de B2B-website (ID 2) zijn
-        if request.website.id == 2 and request.env.user:
-            user = request.env.user
-            partner = user.partner_id
+        # 1. We halen de ID van de partner van de aangemelde gebruiker op (veilige stap)
+        partner_id = user.partner_id.id
+        
+        # 2. We halen de Partner record op, maar nu met Superuser rechten (sudo())
+        # Dit omzeilt de ACL-check voor de leesoperatie (die de write-fout veroorzaakt).
+        PartnerSudo = request.env['res.partner'].sudo()
+        partner_record = PartnerSudo.browse(partner_id)
+        
+        # Controleer of de partner bestaat en of deze niet geblokkeerd is
+        if not partner_record or partner_record.x_is_b2b_blocked:
+            return
+
+        # 3. Haal de prijslijst op met de geforceerde context (pricelist=False)
+        # We gebruiken de partner_record met sudo rechten, zodat de berekening slaagt.
+        pricelist = partner_record.with_context(pricelist=False).property_product_pricelist
+        
+        # 4. Controleer of de prijslijst B2B is
+        if pricelist and pricelist.x_is_b2b_pricelist:
             
-            # Gebruik de gefixeerde logica om de partner prijslijst te krijgen,
-            # door de pricelist uit de context te negeren.
-            pricelist = partner.with_context(pricelist=False).property_product_pricelist
+            # Injecteer de prijslijst ID in de request context
+            request.update_context(pricelist=pricelist.id)
             
-            # Aanname: is_b2b_pricelist is het veld op product.pricelist
-            if pricelist and pricelist.x_is_b2b_pricelist and not partner.x_is_b2b_blocked:
-                
-                # Forceer de pricelist ID in de context van de request
-                # Hierdoor gebruiken alle ORM-berekeningen (zoals product.price) deze pricelist.
-                request.update_context(pricelist=pricelist.id)
-                _logger.info("B2B CONTEXT: Pricelist ID %s van partner %s toegepast op request context.", pricelist.id, partner.name)
-            elif user.id != request.env.ref('base.public_user').id:
-                # Als de gebruiker is ingelogd maar geen B2B-prijslijst heeft,
-                # zorgen we dat de website's standaardprijslijst wordt gebruikt 
-                # (maar de QWeb verbergt de prijs, zie stap 3).
-                _logger.info("B2B CONTEXT: Ingelogde gebruiker zonder B2B-prijslijst. Standaard website pricelist gebruikt.")
-
-
-    # Override voor Categorie/Shop overzichtspagina's
-    @http.route([
-        '/shop',
-        '/shop/page/<int:page>',
-        '/shop/category/<model("product.public.category"):category>',
-        '/shop/category/<model("product.public.category"):category>/page/<int:page>'
-    ], type='http', auth="public", website=True)
-    def shop(self, **post):
-        self._set_b2b_pricelist_context()
-        return super(WebsiteSaleB2B, self).shop(**post)
-
-    # Override voor Product Detailpagina's
-    @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
-    def product(self, product, category='', search='', **kwargs):
-        self._set_b2b_pricelist_context()
-        return super(WebsiteSaleB2B, self).product(product, category=category, search=search, **kwargs)
+            _logger.info("B2B CONTEXT: Pricelist ID %s van partner %s toegepast op request context (FIXED).", pricelist.id, partner_record.name)
+        else:
+             _logger.info("B2B CONTEXT: Partner %s heeft geen B2B-prijslijst. Standaard website pricelist gebruikt.", partner_record.name)
